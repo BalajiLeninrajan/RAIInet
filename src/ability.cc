8345bbd2e2bfa51d194008e761f50e6e4076853f
Tyler Chen <tyche4490@gmail.com>
2025-07-26 21:52:01 -0400

    ability.cc file

---
+++ src/ability.cc
@@ -0,0 +1,33 @@
+#include "ability.h"
+
+void FirewallAbility::use(const std::vector<std::string> &params) {
+}
+
+void DownloadAbility::use(const std::vector<std::string> &params) {
+
+}
+
+void LinkBoostAbility::use(const std::vector<std::string> &params) {
+
+}
+
+void PolarizeAbility::use(const std::vector<std::string> &params) {
+
+}
+
+void ScanAbility::use(const std::vector<std::string> &params) {
+
+}
+
+void BadConnectionAbility::use(const std::vector<std::string> &params){
+
+}
+
+void QuantumEntanglementAbility::use(const std::vector<std::string> &p)
+{
+
+}
+
+void NetworkAbility::use(const std::vector<std::string> &params) {
+
+}
