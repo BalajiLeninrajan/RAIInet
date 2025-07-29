8e88fb1e676f0839e904cf52e2e4bed1c1ffa8b3
Balaji <bleninra@uwaterloo.ca>
2025-07-28 23:21:50 -0400

    link (#20)

--- src/factories.cc
+++ src/factories.cc
@@ -30,12 +30,15 @@ std::unique_ptr<Ability> AbilityFactory::createPlayerAbility(char id) {
 }
 
 std::unique_ptr<Link> LinkFactory::createLink(std::string id,
-                                              std::pair<int, int> startCoords) {
+                                              std::pair<int, int> startCoords,
+                                              Player* owner, Board* board) {
     switch (id[0]) {
         case 'V':
-            return std::make_unique<VirusLink>(startCoords, id[1] - '0');
+            return std::make_unique<VirusLink>(startCoords, id[1] - '0', owner,
+                                               board);
         case 'D':
-            return std::make_unique<DataLink>(startCoords, id[1] - '0');
+            return std::make_unique<DataLink>(startCoords, id[1] - '0', owner,
+                                              board);
         default:
             throw std::invalid_argument("Invalid link id");
     }
