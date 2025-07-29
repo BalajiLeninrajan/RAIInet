17538af3ce18f0c6dba5ac8566cae0ea06c6356e
Kayla <kayla76815@gmail.com>
2025-07-28 23:04:29 -0400

    fixed link.cc

--- src/link.cc
+++ src/link.cc
@@ -11,7 +11,6 @@ Link::Link(std::pair<int, int> startCoords, int strength, Player* owner,
 
 Link::~Link() {}
 
->>>>>>> origin/main
 int Link::getStrength() const { return strength; }
 
 bool Link::getRevealState() const { return false; }
@@ -21,20 +20,7 @@ std::pair<int, int> Link::getCoords() const { return coords; }
 void Link::setCoords(std::pair<int, int> newCoords) { coords = newCoords; }
 
 void Link::requestMove(Link::Direction dir) {
-    switch (dir) {
-        case Direction::NORTH:
-            board->moveLink(coords, {coords.first + 1, coords.second});
-            break;
-        case Direction::SOUTH:
-            board->moveLink(coords, {coords.first - 1, coords.second});
-            break;
-        case Direction::EAST:
-            board->moveLink(coords, {coords.first, coords.second + 1});
-            break;
-        case Direction::WEST:
-            board->moveLink(coords, {coords.first, coords.second - 1});
-            break;
-    }
+    // TODO: call move link on board
 }
 
 Player* Link::getOwner() const { return owner; }
@@ -42,41 +28,17 @@ Player* Link::getOwner() const { return owner; }
 // VirusLink
 
 VirusLink::VirusLink(std::pair<int, int> startCoords, int strength,
-<<<<<<< HEAD
-<<<<<<< HEAD
-                     std::unique_ptr<Player>& owner,
-                     std::unique_ptr<Board>& board)
-=======
                      Player* owner, Board* board)
->>>>>>> origin/main
-=======
-                     Player* owner, Board* board)
->>>>>>> origin/main
     : Link(startCoords, strength, owner, board) {}
 
 Link::LinkType VirusLink::getType() const { return Link::LinkType::VIRUS; }
 
 // DataLink
 
-<<<<<<< HEAD
-DataLink::DataLink(std::pair<int, int> startCoords, int strength,
-<<<<<<< HEAD
-                   std::unique_ptr<Player>& owner,
-                   std::unique_ptr<Board>& board)
-    : Link(startCoords, strength, owner, board) {}
-
-Link::LinkType DataLink::getType() const { return Link::LinkType::VIRUS; }
-=======
-                   Player* owner,
-=======
 DataLink::DataLink(std::pair<int, int> startCoords, int strength, Player* owner,
->>>>>>> origin/main
                    Board* board)
     : Link(startCoords, strength, owner, board) {}
 
-                   Link::LinkType DataLink::getType() const {
-                       return Link::LinkType::VIRUS;
-                   }
+Link::LinkType DataLink::getType() const { return Link::LinkType::VIRUS; }
 
-                   DataLink::~DataLink() {}
->>>>>>> origin/main
+DataLink::~DataLink() {}
